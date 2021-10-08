edb31e4aac17abac38cbc41c7ad9744f8c6a0663
Ayundamita <48998872+Ayundamita@users.noreply.github.com>
2021-10-08 20:16:57 +0700

    Update home.js

--- src/halaman/home.js
+++ src/halaman/home.js
@@ -39,7 +39,7 @@ const Home = () => {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="lg:text-center">
                         <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-                            Tugas Modul 4
+                            Tugas Akhir
                         </h2>
                         <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                             Kelompok 26
